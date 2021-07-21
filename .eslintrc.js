a453cd96902cc1f488420683a6e7fcdbafa0963d
Kemzi <kemzi9585@gmail.com>
2021-07-21 14:55:32 +0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+   parser: '@babel/eslint-parser',
+   parserOptions: {
+      babelOptions: {
+         configFile: './babel.config.json'
+      }
+   },
+   env: {
+      browser: true,
+      node: true,
+      es6: true
+   },
+   extends: [
+      "eslint:recommended"
+   ]
+}
